f6dc0463a680fcd44aaf51bae93910c01ed04958
zdurham <zdurham101@gmail.com>
2024-08-19 23:07:48 -0400

    fleshing out lexer methods in rust

--- rust/src/lexer/lexer.rs
+++ rust/src/lexer/lexer.rs
@@ -1,3 +1,5 @@
+use std::{collections::HashMap, str};
+
 pub enum Token {
     ILLEGAL,
     EOF,
@@ -9,6 +11,8 @@ pub enum Token {
     SEMICOLON,
     LPAREN,
     RPAREN,
+    LBRACE,
+    RBRACE,
     FUNCTION,
     LET,
     MINUS,
@@ -49,9 +53,91 @@ impl Lexer {
             self.ch = 0;
         } else {
             self.ch = *self.input.get(self.read_position).unwrap();
-            //self.readPosition).unwrap_or('0');
         }
         self.position = self.read_position;
+        self.read_position += 1;
+    }
+
+    fn skip_whitespace(&mut self) {
+        while self.ch == b'\r' || self.ch == b' ' || self.ch == b'\n' || self.ch == b'\t' {
+            self.read_char();
+        }
+    }
+    fn next_token(&mut self) -> Token {
+        self.skip_whitespace();
+        let token = match self.ch {
+            b';' => Token::SEMICOLON,
+            b'(' => Token::LPAREN,
+            b')' => Token::RPAREN,
+            b',' => Token::COMMA,
+            b'+' => Token::PLUS,
+            b'{' => Token::LBRACE,
+            b'}' => Token::RBRACE,
+            b'/' => Token::SLASH,
+            b'=' => {
+                if self.peek_char() == b'=' {
+                    self.read_char();
+                    return Token::EQ;
+                }
+                return Token::ASSIGN;
+            }
+            b'!' => {
+                if self.peek_char() == b'=' {
+                    self.read_char();
+                    return Token::NOT_EQ;
+                }
+                return Token::BANG;
+            }
+            b'*' => Token::ASTERISK,
+            b'-' => Token::MINUS,
+            b'>' => Token::GT,
+            b'<' => Token::LT,
+            b'a'..=b'z' | b'A'..=b'Z' | b'_' => {
+                let ident = self.read_identifier();
+                return match ident {
+                    "let" => Token::LET,
+                    "fn" => Token::FUNCTION,
+                    "return" => Token::RETURN,
+                    "if" => Token::IF,
+                    "else" => Token::ELSE,
+                    "true" => Token::TRUE,
+                    "false" => Token::FALSE,
+                    _ => Token::IDENT(ident.into()),
+                };
+            }
+            b'0'..=b'9' => {
+                return Token::INT(self.read_digit().into());
+            }
+            0 => Token::EOF,
+            _ => Token::ILLEGAL,
+        };
+
+        return token;
+    }
+
+    fn peek_char(&self) -> u8 {
+        if self.read_position >= self.input.len() {
+            return 0;
+        } else {
+            return *self.input.get(self.read_position).unwrap();
+        }
+    }
+    fn read_digit(&mut self) -> &str {
+        let initial_position = self.position;
+        while self.ch.is_ascii_digit() {
+            self.read_char()
+        }
+        // cursed?
+        return str::from_utf8(&self.input[initial_position..self.position]).unwrap();
+    }
+
+    fn read_identifier(&mut self) -> &str {
+        let initial_position = self.position;
+        while self.ch.is_ascii_alphabetic() {
+            self.read_char()
+        }
+        // cursed?
+        return str::from_utf8(&self.input[initial_position..self.position]).unwrap();
     }
 }
 
@@ -67,5 +153,16 @@ mod tests {
         assert_eq!(lexer.position, 0);
         assert_eq!(lexer.read_position, 0);
         assert_eq!(lexer.ch, 0);
+    }
+
+    #[test]
+    fn read_char() {
+        let input = "ok";
+        let mut lexer = Lexer::init(input.into());
+        // now testing that read_char works
+        lexer.read_char();
+        assert_eq!(lexer.position, 0);
+        assert_eq!(lexer.read_position, 1);
+        assert_eq!(lexer.ch, b'o');
     }
 }

fb63ed55f1d4e288103341d573258e68683f90b9
zdurham <zdurham101@gmail.com>
2025-01-05 11:08:44 -0500

    toString on expressions

--- src/parser/ast.zig
+++ src/parser/ast.zig
@@ -5,35 +5,83 @@ const lexer = @import("../lexer/lexer.zig");
 pub const LetStatement = struct {
     token: lexer.Token = undefined,
     name: []const u8 = undefined,
-    // value: ?Expression TODO: implement expressions
+    value: ?ExpressionStatement = undefined,
 
     pub fn tokenLiteral(self: LetStatement) []const u8 {
         return self.token.literal;
+    }
+
+    pub fn toString(self: LetStatement, writer: anytype) !void {
+        try writer.write(self.tokenLiteral());
+        try writer.write(" ");
+        try writer.write(self.name);
+        try writer.write(" = ");
+        // try writer.write(
+        if (self.value) |_| {
+            std.debug.print("ok we have an expr", .{});
+        }
     }
 };
 
 pub const ReturnStatement = struct {
     token: lexer.Token,
-    // returnValue: Expression, // TODO: implement expression
+    returnValue: ?ExpressionStatement = undefined,
     pub fn tokenLiteral(self: ReturnStatement) []const u8 {
         return self.token.literal;
+    }
+
+    pub fn toString(self: ReturnStatement, writer: anytype) !void {
+        try writer.write(self.token.literal);
+        try writer.write(" ");
+        if (self.returnValue) |rValue| {
+            try writer.write(rValue.toString(writer));
+        }
+        try writer.write(";");
+    }
+};
+
+pub const Expression = union(enum) {
+    token: lexer.Token,
+    // TODO: enumerate various expressions
+    pub fn toString(self: Expression, writer: anytype) void {
+        return switch (self) {
+            inline else => |case| case.toString(writer),
+        };
+    }
+};
+
+pub const ExpressionStatement = struct {
+    token: lexer.Token,
+    expression: Expression,
+
+    pub fn tokenLiteral(self: ExpressionStatement) []const u8 {
+        return self.token.literal;
+    }
+
+    pub fn toString(self: ExpressionStatement, writer: anytype) void {
+        if (self.expression) |es| {
+            return es.toString(writer);
+        }
+        return "";
     }
 };
 
 pub const Statement = union(enum) {
     letStatement: LetStatement,
     returnStatement: ReturnStatement,
+    expressionStatement: ExpressionStatement,
 
     pub fn tokenLiteral(self: Statement) []const u8 {
         return switch (self) {
             inline else => |case| case.tokenLiteral(),
         };
     }
-};
 
-const Expression = struct {
-    pub fn tokenLiteral() []const u8 {}
-    pub fn expressionNode() void {}
+    pub fn toString(self: Statement, writer: anytype) []const u8 {
+        return switch (self) {
+            inline else => |case| case.string(writer),
+        };
+    }
 };
 
 pub const Program = struct {
@@ -52,11 +100,19 @@ pub const Program = struct {
         self.statements.deinit();
     }
 
-    fn get_token_literal(self: *Self) []const u8 {
-        if (self.statements.len > 0) {
-            return self.statements[0].tokenLiteral();
+    fn getTokenLiteral(self: *Self) []const u8 {
+        if (self.statements.items.len > 0) {
+            return self.statements.items[0].tokenLiteral();
         } else {
             return "";
+        }
+    }
+
+    // we'll pass an arbitrary writer() in to this function
+    // and all children will use it as well
+    pub fn toString(self: *Self, writer: anytype) !void {
+        for (self.statements.items) |stmt| {
+            try stmt.toString(writer);
         }
     }
 };
